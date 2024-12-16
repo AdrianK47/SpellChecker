5bb903715c1486e449dd424090cf0ebc8e116950
AdrianK47 <114188316+AdrianK47@users.noreply.github.com>
2024-12-16 14:37:08 -0500

    hehehe

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ class Main {
   public static void main(String[] args) {
     SpellChecker checker = new SpellChecker();
     checker.print10();
-    checker.printStartsWith("ab");
+    //checker.printStartsWith("ab");
     System.out.print("Enter a word to spell check or q to quit: ");
     Scanner scan = new Scanner(System.in);
     String word = scan.nextLine();
-    while (!word.equals("q")) {
+    /*while (!word.equals("q")) {
       if (checker.spellcheck(word) == true)
         System.out.println(word + " is spelled correctly!");
       else
@@ -16,7 +16,7 @@ class Main {
 
       System.out.print("Enter a word to spell check or q to quit: ");
       word = scan.nextLine();
-      }    
+      }  */  
     scan.close();
   }
 }
